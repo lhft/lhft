5e568d42e8c616f155106106bb86e7ddbdc76057
Azku <asier@zebixe.com>
2012-09-06 12:05:40 +0200

    Feature, the script now works for Basque and spanish. Basque it's main. Added Spanish text as well

--- transform.sh
+++ transform.sh
@@ -1,10 +1,60 @@
-#!/bin/bash
+#! /bin/bash
+#: Title       : Tenasform
+#: Date        : 2011-12-05
+#: Description : Starting from bone documents, blends them with 
+#                pieces of html scattered in different html files
 
-for i in transform_*.html
+
+blend_eu(){
+    for i in body_eu_*.html
+    do
+	f="$(echo "$i" | cut -d"_" -f3)"
+	echo "Blending $i into $f"
+	sed "/:-)main-body/{r $i
+;d}" <bones_eu.azkuml | sed "/:-)news/{r news_eu.html
+;d}" > "$f"
+    done 
+}
+blend_es(){
+    for i in body_es_*.html
+    do
+	f="$(echo "$i" | cut -d"_" -f3)"
+	n="$(echo "$f" | cut -d"." -f1)es.html"
+
+	echo "Blending $i into $n"
+	sed "/:-)main-body/{r $i
+;d}" <bones_es.azkuml | sed "/:-)news/{r news_es.html
+;d}" > "$n"
+    done 
+}
+
+
+while getopts ":h" i
 do
-f="$(echo "$i" | cut -d"_" -f2)"
-echo $f
-sed "/:-)main-copy/{r $i
-;d}" <bones.azkuml > "$f"
-done 
+    case $i in
+        h)
+            cat << EOF
 
+                Starting from bone documents, blends them with 
+                pieces of html scattered in different html files
+
+-h     show this help 
+
+EOF
+            exit 0
+            ;;
+        f)
+            FILENAME=$OPTARG
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument.">&2
+            exit 1
+            ;;
+        \?)
+            echo "Parameter not defined $OPTARG" >&2
+            exit 1
+            ;;
+        esac
+done
+blend_eu
+blend_es
